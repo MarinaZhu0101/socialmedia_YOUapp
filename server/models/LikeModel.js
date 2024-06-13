ea2898f77a20534c98ce2e588db8d066639c4696
MarinaZhu0101 <126560202+MarinaZhu0101@users.noreply.github.com>
2024-06-13 18:37:27 +0200

    Add files via upload

--- server/models/LikeModel.js
+++ server/models/LikeModel.js
@@ -1,19 +1,61 @@
-const connection = require('./db.js');
+// const connection = require('./db.js');
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const likeSchema = new Schema({
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    post_id: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+});
+
+const Like = mongoose.model('Like', likeSchema);
 
 class LikeModel {
+    // static getLikesByPostId(postId, callback) {
+    //     const query = "SELECT user_id FROM likes WHERE post_id = ?";
+    //     connection.query(query, [postId], (error, results) => {
+    //         if (error) {
+    //             console.log("Error in getLikesDetailByPostId:", error);
+    //             return callback(error, null);
+    //         }
+      
+    //         const userIds = results.map(row => row.user_id);
+    //         callback(null, userIds);
+    //     });
+    // }
     static getLikesByPostId(postId, callback) {
-        const query = "SELECT user_id FROM likes WHERE post_id = ?";
-        connection.query(query, [postId], (error, results) => {
+        Like.find({ post_id: postId }, 'user_id', (error, results) => {
             if (error) {
-                console.log("Error in getLikesDetailByPostId:", error);
+                console.log("Error in getLikesByPostId:", error);
                 return callback(error, null);
             }
-      
-            const userIds = results.map(row => row.user_id);
+            const userIds = results.map(like => like.user_id);
             callback(null, userIds);
         });
     }
 
+    // static addLike(likeData, callback) {
+    //     this.checkUserLike(likeData.user_id, likeData.post_id, (error, hasLiked) => {
+    //         if (error) {
+    //             console.error("Error checking user like:", error);
+    //             return callback(error);
+    //         }
+    //         if (hasLiked) {
+    //             console.log("User already liked this post.");
+    //             return callback(null, { alreadyLiked: true, message: "User already liked this post." });
+    //         } else {
+    //             const query = "INSERT INTO likes SET ?";
+    //             connection.query(query, likeData, (error, results) => {
+    //                 if (error) {
+    //                     console.error("Error in addLike:", error);
+    //                     return callback(error, null);
+    //                 }
+    //                 console.log("Add like successful");
+    //                 callback(null, { success: true, message: "Like added successfully.", results });
+    //             });
+    //         }
+    //     });
+    // }
     static addLike(likeData, callback) {
         this.checkUserLike(likeData.user_id, likeData.post_id, (error, hasLiked) => {
             if (error) {
@@ -24,22 +66,44 @@ class LikeModel {
                 console.log("User already liked this post.");
                 return callback(null, { alreadyLiked: true, message: "User already liked this post." });
             } else {
-                const query = "INSERT INTO likes SET ?";
-                connection.query(query, likeData, (error, results) => {
+                const like = new Like(likeData);
+                like.save((error, result) => {
                     if (error) {
                         console.error("Error in addLike:", error);
                         return callback(error, null);
                     }
                     console.log("Add like successful");
-                    callback(null, { success: true, message: "Like added successfully.", results });
+                    callback(null, { success: true, message: "Like added successfully.", result });
                 });
             }
         });
     }
 
 
-    static removeLike(userId, postId, callback) {
+    // static removeLike(userId, postId, callback) {
 
+    //     this.checkUserLike(userId, postId, (error, hasLiked) => {
+    //         if (error) {
+    //             console.error("Error checking user like before removal:", error);
+    //             return callback(error);
+    //         }
+    //         if (!hasLiked) {
+    //             console.log("No like found to remove.");
+    //             return callback(null, { notFound: true, message: "Like not found." });
+    //         } else {
+    //             const query = "DELETE FROM likes WHERE user_id = ? AND post_id = ?";
+    //             connection.query(query, [userId, postId], (error, results) => {
+    //                 if (error) {
+    //                     console.error("Error in removeLike:", error);
+    //                     return callback(error);
+    //                 }
+    //                 console.log("Remove like successful");
+    //                 callback(null, { success: true, message: "Like removed successfully.", results });
+    //             });
+    //         }
+    //     });
+    // }
+    static removeLike(userId, postId, callback) {
         this.checkUserLike(userId, postId, (error, hasLiked) => {
             if (error) {
                 console.error("Error checking user like before removal:", error);
@@ -49,30 +113,37 @@ class LikeModel {
                 console.log("No like found to remove.");
                 return callback(null, { notFound: true, message: "Like not found." });
             } else {
-                const query = "DELETE FROM likes WHERE user_id = ? AND post_id = ?";
-                connection.query(query, [userId, postId], (error, results) => {
+                Like.deleteOne({ user_id: userId, post_id: postId }, (error, result) => {
                     if (error) {
                         console.error("Error in removeLike:", error);
                         return callback(error);
                     }
                     console.log("Remove like successful");
-                    callback(null, { success: true, message: "Like removed successfully.", results });
+                    callback(null, { success: true, message: "Like removed successfully.", result });
                 });
             }
         });
     }
     
+    // static checkUserLike(userId, postId, callback) {
+    //     const query = "SELECT * FROM likes WHERE user_id = ? AND post_id = ?";
+    //     connection.query(query, [userId, postId], (error, results) => {
+    //         if (error) {
+    //             console.log("Error in checkUserLike:", error);
+    //         }
+    //         const hasLiked = results.length > 0;
+    //         callback(error, hasLiked);
+    //     });
+    // }
     static checkUserLike(userId, postId, callback) {
-        const query = "SELECT * FROM likes WHERE user_id = ? AND post_id = ?";
-        connection.query(query, [userId, postId], (error, results) => {
+        Like.findOne({ user_id: userId, post_id: postId }, (error, result) => {
             if (error) {
                 console.log("Error in checkUserLike:", error);
             }
-            const hasLiked = results.length > 0;
+            const hasLiked = !!result;
             callback(error, hasLiked);
         });
     }
-
 
 }
 
