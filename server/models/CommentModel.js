d0b1aa7866684b982d192ee95502d072f10a61a2
MarinaZhu0101 <126560202+MarinaZhu0101@users.noreply.github.com>
2024-06-13 18:30:07 +0200

    Add files via upload

--- server/models/CommentModel.js
+++ server/models/CommentModel.js
@@ -1,26 +1,43 @@
-const connection = require('./db.js');
+// const connection = require('./db.js');
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+    post_id: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    created_at: { type: Date, default: Date.now }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
 
 class CommentModel {
+    // static getCommentsByPostId(postId, callback) {
+    //     const query = `
+    //     SELECT comments.*, users.user_name 
+    //     FROM comments 
+    //     INNER JOIN users ON comments.user_id = users.user_id
+    //     WHERE comments.post_id = ?`;
+    //     connection.query(query, [postId], callback);
+    // }
     static getCommentsByPostId(postId, callback) {
-        const query = `
-        SELECT comments.*, users.user_name 
-        FROM comments 
-        INNER JOIN users ON comments.user_id = users.user_id
-        WHERE comments.post_id = ?`;
-        connection.query(query, [postId], callback);
+        Comment.find({ post_id: postId }).populate('user_id', 'user_name').exec(callback);
     }
 
+    // static createComment(commentData, callback) {
+    //     const query = "INSERT INTO comments SET ?";
+    //     connection.query(query, commentData, (error, results) => {
+    //         if (error) {
+    //             console.log("Error in createComment:", error);
+    //         }
+    //         callback(error, results);
+    //     });
+    // }  
     static createComment(commentData, callback) {
-        const query = "INSERT INTO comments SET ?";
-        connection.query(query, commentData, (error, results) => {
-            if (error) {
-                console.log("Error in createComment:", error);
-            }
-            callback(error, results);
-        });
+        const comment = new Comment(commentData);
+        comment.save(callback);
     }
-    
-
 }
 
 module.exports = CommentModel;
--- server/models/db.js
+++ server/models/db.js
@@ -1,13 +1,32 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+
+// const DBCONFIG = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     port: process.env.DB_PORT,
+// };
+
+// const connection = mysql.createConnection(DBCONFIG);
+
+// module.exports = connection;
+
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const DBCONFIG = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    uri: process.env.DB_URI,
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
 };
 
-const connection = mysql.createConnection(DBCONFIG);
+mongoose.connect(DBCONFIG.uri, DBCONFIG.options)
+    .then(() => console.log('MongoDB connection successful'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = connection;
+module.exports = mongoose;
+
+
