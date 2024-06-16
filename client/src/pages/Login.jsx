3ff71b7bd49b60a7240e70d981c5eef40c14831b
MarinaZhu0101 <126560202+MarinaZhu0101@users.noreply.github.com>
2024-06-16 17:27:01 +0100

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
   const handleRegister = (e) => {
       e.preventDefault();
   
-      axios.post(`${apiUrl}/auth/login`, { username, password })
+      axios.post('https://you-app-tau.vercel.app/auth/login', { username, password })
         .then(res => {
           sessionStorage.setItem("accessToken", res.data.token);
           setAuthState(true);
