427804261c888400f39fd2f75a155d51de37b7f7
MarinaZhu0101 <126560202+MarinaZhu0101@users.noreply.github.com>
2024-06-16 20:56:46 +0100

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -56,7 +56,7 @@ function CreatePost() {
       return;
     }
 
-    axios.post('https://you-app-tau.vercel.app/posts', formdata, {
+    axios.post('https://you-app-backend.vercel.app/posts', formdata, {
       headers: {
         'Authorization': `Bearer ${accessToken}`, 
       }
